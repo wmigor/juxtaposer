ef77c3d10ab3c7bef24899c01e8663b08ff38500
wmigor <wmigor@yandex.ru>
2018-06-16 13:00:44 +0300

    fix

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ setup(
 	name="juxtaposer",
 	version="0.1",
 	author="Igor Akulov",
-	author_email = "wmigor@yandex.ru",
+	author_email="wmigor@yandex.ru",
 	packages=find_packages(),
 	entry_points={
 		"gui_scripts": ["juxtaposer = juxtaposer.main:main"]
